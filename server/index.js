568749473c8cbc397e223c652eb8be878f011dfa
Raushan Kumar <146509821+raushgit@users.noreply.github.com>
2025-08-29 23:39:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const router = require('./routes/auth');
  
 const port = process.env.PORT || 5000 ;
- 
+
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
